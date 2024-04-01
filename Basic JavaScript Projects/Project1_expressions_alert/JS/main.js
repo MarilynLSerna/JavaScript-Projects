406ae7137d09f461d0f257b5399b11df78f57a86
MarilynLSerna <160423192+MarilynLSerna@users.noreply.github.com>
2024-03-31 19:59:48 -0700

    JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+document.write("Hello, World! ");
+var A = "This is a string. ";
+document.write(A);
+
+//Assign a string value to a variable//
+var X = "Greetings!"; 
+alert(X);
+
+//Adding escape fro quotes and concatenating a string//
+document.write("Moira said to David, \"Next step is to fold in the cheese.\" David looks at Miora \"What does that mean?"
++ " What does fold in the cheese mean?\" He responded. "); 
+
+//concatenate a string to a variable//
+var M = "I am a" + " Gemini."; 
+document.write(M);
+
+//Multiple variables, having one variable displayed//
+var Flowers = "Types of flowers", Pink = "Azalea", Blue = "Iris", Red = "Rose", Purple = "Gladiolus"; 
+document.write(Pink);
+
+//Values made into an expression//
+document.write(6 + 7);
