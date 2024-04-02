c2bc2aa88fcd589f77abc57ec14856c4afe9cd2b
MarilynLSerna <160423192+MarilynLSerna@users.noreply.github.com>
2024-04-02 16:00:44 -0700

    Projects on Functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+//This is a button that displays a var string//
+function My_First_Function() {
+    var str = "This is my button :)";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+//This shows the string when the mouse hovers over the text//
+function My_Function() {
+    var sentence = "This is my";
+    sentence += " operator string!";
+    document.getElementById("Text_Hover").innerHTML = sentence;
+}
