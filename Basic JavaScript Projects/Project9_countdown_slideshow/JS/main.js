232b3758010698b81b1fb08c1a2fd4d90422091a
MarilynLSerna <160423192+MarilynLSerna@users.noreply.github.com>
2024-04-11 20:20:21 -0700

    Countdown  Slideshow

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,47 @@
+// Function to handle countdown timer
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    //Functin to decrease the timer and update the display
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick(); // Start the countdown
+}
+
+// Initialize slide index and get slide elements
+let slideIndex = 1;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Function to display the current slide
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
